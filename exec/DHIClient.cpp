daf64e06f1113d0e7a1a73c9c8e9585bb08eff60
Ytinifni-zc <zhcheng127@gmail.com>
2021-04-21 11:12:06 +0800

    Init DistributedHashIndexing

---
+++ exec/DHIClient.cpp
@@ -0,0 +1,8 @@
+//
+// Created by 赵程 on 2021/4/21.
+//
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
---
+++ exec/DHIServer.cpp
@@ -0,0 +1,10 @@
+//
+// Created by 赵程 on 2021/4/21.
+//
+
+#include <rpc/rpc.h>
+
+int main(int argc, char *argv[]) {
+
+    return 0;
+}
